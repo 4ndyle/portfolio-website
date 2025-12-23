bf47283c14e248ab1dadaa442327edd4a024fc68
Andy Le <4ndyle@gmail.com>
2025-12-23 15:57:34 -0700

    feat: Add encrytion effect to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
+import { EncryptedText } from "./ui/encrypted-text";
+
 function Footer() {
   return (
     <section className="mt-96 pb-48 w-full">
-      <div>
-        <h2 className="text-7xl font-bold my-16 text-left mb-4 max-[500px]:text-5xl max-[550px]:text-center">
+      <div className="flex flex-col">
+        {/* <h2 className="text-7xl font-bold my-16 text-left mb-4 max-[500px]:text-5xl max-[550px]:text-center">
           The End.
-        </h2>
+        </h2> */}
+        <EncryptedText
+          text="The End."
+          encryptedClassName="text-neutral-500"
+          revealedClassName="dark:text-white text-black"
+          revealDelayMs={150}
+          className="text-7xl font-bold my-16 text-left mb-4 max-[500px]:text-5xl max-[550px]:text-center"
+        />
         <p className="text-left text-[#B4B4B4] text-xl max-[550px]:text-center max-[500px]:text-lg">
           {" "}
           4ndyle@gmail.com{" "}
